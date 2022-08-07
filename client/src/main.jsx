0a14e7efbfcf6f686fbc823f263429b193e9979a
kirsanovandrey <AKirsanov@inspirebrands.com>
2022-08-07 16:00:07 +0300

    last changes

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,15 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import App from "./App";
+import { TransactionsProvider } from "./context/TransactionContext";
 import "./index.css";
 
-import { TransactionProvider } from  './context/TransactionContext';
-
 ReactDOM.render(
-  <TransactionProvider>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </TransactionProvider>,
+  <TransactionsProvider>
+    <App />
+  </TransactionsProvider>,
   document.getElementById("root"),
 );
